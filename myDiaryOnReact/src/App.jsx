a23835cb36ceb7cec5296d8d51dc65e3988d63c3
alex <alexnikolaenko@yandex.ru>
2024-06-04 17:28:29 +1000

    start video 24

--- myDiaryOnReact/src/App.jsx
+++ myDiaryOnReact/src/App.jsx
@@ -1,14 +1,53 @@
 import "./App.css"
 import Button from "./components/Button/Button"
+import CardButton from "./components/CardButton/CardButton"
 import JournalItem from "./components/JournalItem/JournalItem"
 
 function App() {
+  const data = [
+    {
+      title: "Подготовка к обновлению курсов",
+      text: "Горгые походы открывают удивительные природные ландшафты",
+      date: new Date()
+    },
+    {
+      title: "Поход в горы",
+      text: "Думал, чт оочень много времени",
+      date: new Date()
+    },
+    {
+      title: "Подготовка к экзаменам",
+      text: "Я молодец, я всё знаю. Сдам на пять!",
+      date: new Date()
+    }
+  ]
+
   return (
     <>
       <h1>Caption</h1>
       <p>Same text</p>
-      <Button />
-      <JournalItem />
+    <CardButton>Новое воспоминание</CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[0].title}
+          date={data[0].date}
+          text={data[0].text}
+        />
+      </CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[1].title}
+          date={data[1].date}
+          text={data[1].text}
+        />
+      </CardButton>
+      <CardButton>
+        <JournalItem
+          title={data[2].title}
+          date={data[2].date}
+          text={data[2].text}
+        />
+      </CardButton>
     </>
   )
 }
