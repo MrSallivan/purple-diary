0d0bc4f40a6318d2808105fdd01907a80d2972d8
alex <alexnikolaenko@yandex.ru>
2024-06-04 13:59:17 +1000

    start 21 video props

--- myDiaryOnReact/src/App.jsx
+++ myDiaryOnReact/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css"
+import Button from "./components/Button/Button"
+import JournalItem from "./components/JournalItem/JournalItem"
 
 function App() {
   return (
     <>
       <h1>Caption</h1>
       <p>Same text</p>
+      <Button />
+      <JournalItem />
     </>
   )
 }
